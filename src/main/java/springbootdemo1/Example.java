81fba5d8df3a4788f8aa67e965e60634d12c5f46
ly217 <liyi_217@163.com>
2017-04-22 16:23:45 +0800

    我的第一个版本

---
+++ src/main/java/springbootdemo1/Example.java
@@ -0,0 +1,26 @@
+package springbootdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ Classname: Example
+ * @ Description:
+ * @ author: liyi
+ * @ date: 2017/4/22 15:40
+ * @ version: V1.0
+ */
+@RestController
+@EnableAutoConfiguration
+public class Example {
+    @RequestMapping("/")
+     String home(){
+         return "Hello World11111!";
+     }
+
+     public static  void main(String args[]){
+         SpringApplication.run(Example.class,args);
+     }
+}
